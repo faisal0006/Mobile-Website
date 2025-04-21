2e11d4c5926a425fae66c20fbfb55d90e7244ee8
Md Faisal <98255055+faisal0006@users.noreply.github.com>
2025-04-21 18:59:45 +0530

    Budget Mobiles

---
+++ src/budget.js
@@ -0,0 +1,61 @@
+const Budget = [
+  {
+    name: "Infinix Note 50x",
+    img: "https://rukminim2.flixcart.com/image/850/1000/xif0q/mobile/c/s/y/note-50x-5g-x6857b-infinix-original-imahanf7nru4zqnj.jpeg?q=90&crop=false",
+    price: "11,499"
+  },
+  {
+    name: "Samsung Galaxy F16",
+    img: "https://rukminim3.flixcart.com/image/850/1000/xif0q/mobile/5/5/1/-original-imahakzshfjuhhtj.jpeg?q=90&crop=false",
+    price: "11,499"
+  },
+  {
+    name: "Nothing CMF Phone 1",
+    img: "https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1740667875/Croma%20Assets/Communication/Mobiles/Images/308337_0_qllzdj.png?tr=w-600",
+    price: "13,999"
+  },
+  {
+    name: "Poco M7 Pro",
+    img: "https://rukminim2.flixcart.com/image/850/1000/xif0q/mobile/v/m/x/-original-imah7jspd8dcxfut.jpeg?q=20&crop=false",
+    price: "13,999"
+  },
+  {
+    name: "Infinix Note 30 5G",
+    img: "https://rukminim2.flixcart.com/image/720/864/xif0q/mobile/u/t/k/note-30-5g-x6711-infinix-original-imagqdvvw4pwdtmk.jpeg?q=60&crop=false",
+    price: "14,999"
+  },
+  {
+    name: "Realme P3",
+    img: "https://www.themobileindian.com/wp-content/uploads/2025/03/Realme-P3-5G-specs-600x600.jpg",
+    price: "16,999"
+  },
+  {
+    name: "Nothing Phone (2a)",
+    img: "https://m.media-amazon.com/images/I/61oycK5i7qL.jpg",
+    price: "17,999"
+  },
+  {
+    name: "iQOO Z9s",
+      img: "https://m.media-amazon.com/images/I/61BxKJrnbdL._SL1200_.jpg",
+      price: "19,999"
+    },
+    {
+      name: "Realme NARZO 80 Pro",
+      img: "https://m.media-amazon.com/images/I/71O+42MuShL._SL1500_.jpg",
+      price: "19,999"
+    },
+    {
+      name: "Motorola Edge 50 Neo",
+      img: "https://m.media-amazon.com/images/I/71KdLCGqEvL.jpg",
+      price: "20,999"
+    },
+  ];
+  
+  export default Budget;
+  
+
+  // {
+  //   name: "Redmi Note 5 Pro",
+  //   img: "https://global.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1518342456.70319411.jpg",
+  //   price: "15000"
+  // }
