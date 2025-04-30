517f964731c965ea134061a0bed507c71b07bc7f
Md Faisal <98255055+faisal0006@users.noreply.github.com>
2025-04-30 14:39:31 +0530

    Update PhoneHistory.jsx

--- src/components/PhoneHistory.jsx
+++ src/components/PhoneHistory.jsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 import { motion, useScroll, useTransform } from 'framer-motion';
-import phoneHistory from '../data/phoneHistory';
+import PhoneHistory from '../data/phoneHistory';
 
 
 const TimelineItem = ({ item, index, containerRef }) => {
